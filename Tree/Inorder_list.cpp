7a3cb81a71e1ebfb67def9a04d56bbd279910314
dubeyaayushi <dubeyaayushi1028@gmail.com>
2025-08-23 20:24:01 +0530

    Inorder traversal of tree

---
+++ Tree/Inorder_list.cpp
@@ -0,0 +1,87 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node{
+    int data; 
+    Node* left;
+    Node* right;
+
+    Node(int value){
+       data = value;
+        left = right = NULL;
+
+    }
+};
+
+
+Node* buildTree(){
+
+    int val; 
+    cout<<"Enter the root value(-1 for no node)"<<endl;
+    cin>>val;
+    if( val == -1)
+    {
+        return NULL;
+    }
+
+    Node* root = new Node(val);
+
+    queue<Node*>q;
+    q.push(root);
+
+    while(!q.empty()){
+
+        Node* current = q.front();
+        q.pop();
+
+        int leftval, rightval;
+
+        cout<<"Enter rthe value for left child of"<<current->data<<"(-1 for no node):";
+        cin>>leftval;
+
+        if(leftval!= -1){
+            current->left = new Node(leftval);
+            q.push(current->left);
+        }
+
+        cout<<"Enter the value for right child of "<<current->data<<"(-1 for no node):";
+        cin>>rightval;
+
+        if(rightval!= -1){
+            current->right = new Node(rightval);
+            q.push(current->right);
+        }
+    }
+
+    return root;
+}
+
+void inorderTraversal(Node* root, vector<int>& inorder) {
+    if (root == NULL) return;
+    inorderTraversal(root->left, inorder);
+    inorder.push_back(root->data);
+    inorderTraversal(root->right, inorder);
+}
+
+int main() {
+    Node* root = buildTree();
+    vector<int> inorder;
+    inorderTraversal(root, inorder);
+    cout << "Inorder Traversal: ";
+    for (int val : inorder) {
+        cout << val << " ";
+    }
+    cout << endl;
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
